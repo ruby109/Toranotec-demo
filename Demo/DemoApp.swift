f80c3930590262c15bdd5e6854b919f6b610df69
Lihong Shen <ruby109lol@gmail.com>
2023-07-17 17:37:51 +0900

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by 沈 立竑 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
